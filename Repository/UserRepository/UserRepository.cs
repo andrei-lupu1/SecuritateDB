c91dbf67898b55b2ab8da7c89c8400532b838924
Andrei Lupu <150147059+andrei-lupu1@users.noreply.github.com>
2024-01-08 09:10:02 +0200

    Update UserRepository.cs

--- Repository/UserRepository/UserRepository.cs
+++ Repository/UserRepository/UserRepository.cs
@@ -22,10 +22,9 @@ namespace Repository.UserRepository
 
         public int Register(string username, string password)
         {
-            int result = 0;
             OracleParameter param1 = new OracleParameter("USERNAME", OracleDbType.Varchar2, username, System.Data.ParameterDirection.Input);
             OracleParameter param2 = new OracleParameter("PASS", OracleDbType.Varchar2, password, System.Data.ParameterDirection.Input);
-            OracleParameter param3 = new OracleParameter("RESULT", OracleDbType.Int32, result, System.Data.ParameterDirection.Output);
+            OracleParameter param3 = new OracleParameter("RESULT", OracleDbType.Int32, System.Data.ParameterDirection.Output);
             OracleParameter[] parameters = new OracleParameter[] { param1, param2, param3 };
             _context.Database.ExecuteSqlRaw("BEGIN REGISTER(:USERNAME, :PASS, :RESULT); END;", parameters);
             return result;
